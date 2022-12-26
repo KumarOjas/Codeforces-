fca2dc6ada13aa7c13880a1186dd2afdce54351d
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-12-25 23:44:02 -0800

    Create DominoPiling.cpp

---
+++ DominoPiling.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+using namepspace std;
+
+int main (){
+  
+   int M = 0, N = 0; 
+  
+    scanf("%d %d\n",&M, &N);
+    
+    printf("%d\n", M * N / 2);
+    
+    return 0;
+}  
+  
