766ff768aef3b65d05a2e1adfcd50c54c72721a3
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2023-01-06 21:20:20 -0800

    Create TheNumberOfPositions.cpp

---
+++ TheNumberOfPositions.cpp
@@ -0,0 +1,16 @@
+
+ #include <cstdio>
+
+ #include <iostream>
+
+
+  int main (){
+  
+  int n,a,b;
+ 
+   scanf("%d %d %d", &n, &a, &b);
+   
+   printf("%d\n", std::min(n - a,b + 1));
+   
+    return 0;
+}
